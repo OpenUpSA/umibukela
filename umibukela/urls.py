2ba1f11d69e66a31743047ce2e87656c170156f0
Greg Kempe <greg@kempe.net>
2015-11-30 12:19:38 +0200

    About, contact, tabs

--- umibukela/urls.py
+++ umibukela/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'umibukela.views.home', name='home'),
+    url(r'^contact$', 'umibukela.views.contact', name='contact'),
+    url(r'^about$', 'umibukela.views.about', name='about'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- umibukela/views.py
+++ umibukela/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'active_tab': 'home',
+    })
+
+
+def contact(request):
+    return render(request, 'contact.html', {
+        'active_tab': 'contact',
+    })
+
+
+def about(request):
+    return render(request, 'about.html', {
+        'active_tab': 'about',
+    })
