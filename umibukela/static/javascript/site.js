311d9ebe3cfe90c8f7112b789f19627f6c6e8639
Greg Kempe <greg@kempe.net>
2015-12-02 14:27:52 +0200

    Historical comparisons

--- umibukela/static/javascript/site.js
+++ umibukela/static/javascript/site.js
@@ -34,7 +34,7 @@ Umibukela.Site = function() {
     Highcharts.setOptions({
       credits: {enabled: false},
       chart: {animation: false},
-      colors: ['#f6921d', '#000'],
+      colors: ['#f6921d', '#ccc'],
       title: {text: null},
       xAxis: {
         lineWidth: 0,
@@ -42,12 +42,16 @@ Umibukela.Site = function() {
       },
       yAxis: {visible: false},
       legend: {enabled: false},
-      tooltip: {enabled: false},
+      tooltip: {
+        formatter: function() {
+          return '<b>' + this.x + ': ' + this.y + '%</b><br>' + this.series.name;
+        }
+      },
       plotOptions: {
         series: {
           animation: false,
-          pointPadding: 0.05,
-          groupPadding: 0,
+          pointPadding: 0.0,
+          groupPadding: 0.07,
         },
         column: {
           dataLabels: {
@@ -61,34 +65,56 @@ Umibukela.Site = function() {
     var data = {
       chart1: {
         labels: ['Yes', 'No'],
-        values: [88, 109],
+        values: {
+          current: [88, 109],
+          historical: [132, 83],
+        },
       },
       chart2: {
         labels: ['Yes', 'No'],
-        values: [64, 43],
+        values: {
+          current: [64, 43],
+          historical: [53, 41],
+        },
       },
       chart3: {
         labels: ['Positive', 'Neutral', 'Negative'],
-        values: [65, 36, 96],
+        values: {
+          current: [65, 36, 96],
+          historical: [34, 27, 152],
+        }
       },
       chart4: {
         labels: ['Positive', 'Neutral', 'Negative'],
-        values: [46, 28, 33],
+        values: {
+          current: [46, 28, 33],
+          historical: [14, 9, 72],
+        },
       },
       chart5: {
         labels: ['More than 4 hours', '1 - 2 hours', '3 - 4 hours', '2 - 3 hours', 'Less than 1 hour'],
-        values: [1, 84, 4, 22, 86],
+        values: {
+          current: [1, 84, 4, 22, 86],
+          historical: [18, 57, 17, 52, 70],
+        },
       },
       chart6: {
         labels: ['More than 4 hours', '1 - 2 hours', '3 - 4 hours', '2 - 3 hours', 'Less than 1 hour'],
-        values: [1, 84, 4, 22, 86],
+        values: {
+          current: [1, 84, 4, 22, 86],
+          historical: [1, 29, 10, 19, 35],
+        },
       },
     };
 
     $('.chart').each(function(i) {
       var d = data[$(this).data('indicator')];
-      var total = _.reduce(d.values, function(s, v){ return s + v; }, 0);
-      var values = _.map(d.values, function(v) { return Math.round(v / total * 100); });
+
+      var currTotal = _.reduce(d.values.current, function(s, v){ return s + v; }, 0);
+      var currValues = _.map(d.values.current, function(v) { return Math.round(v / currTotal * 100); });
+
+      var histTotal = _.reduce(d.values.historical, function(s, v){ return s + v; }, 0);
+      var histValues = _.map(d.values.historical, function(v) { return Math.round(v / histTotal * 100); });
 
       $(this).highcharts({
         chart: {type: 'column'},
@@ -96,7 +122,19 @@ Umibukela.Site = function() {
           categories: d.labels,
           labels: {step: 1},
         },
-        series: [{data: values}],
+        series: [{
+          data: currValues,
+          stack: 'current',
+          name: 'Current cycle',
+        }, {
+          data: histValues,
+          stack: 'historical',
+          pointWidth: 10,
+          name: 'Previous cycles',
+          dataLabels: {
+            enabled: false,
+          },
+        }],
       });
     });
   };
