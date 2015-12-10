b96137a9ac2419298b964bf5c1a8ec3a5e19ef26
Fritz Jooste <fritz@fritzjooste.com>
2015-12-10 12:41:50 +0200

    Added step navigation to about page
    Also added jQuery code for smooth scrolling to page anchors ('#')

--- umibukela/static/javascript/app.js
+++ umibukela/static/javascript/app.js
@@ -37,3 +37,15 @@ $(function() {
   Umibukela.general = new Umibukela.General();
   Umibukela.general.init();
 });
+
+// function for scrolling around website (links to "#section" will scroll to that section)
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-15
+        }, 900, 'swing');
+    });
