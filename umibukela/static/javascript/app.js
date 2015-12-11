96664cdc8072d8545487e05281ebecd63a50b5f0
Greg Kempe <greg@kempe.net>
2015-12-11 08:56:05 +0200

    Affix for about page steps

--- umibukela/static/javascript/app.js
+++ umibukela/static/javascript/app.js
@@ -5,31 +5,69 @@ Umibukela.General = function() {
 
   self.init = function() {
     self.initMaps();
+
+    // function for scrolling around website (links to "#section" will scroll to that section)
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top-15
+      }, 900, 'swing');
+    });
+
+    if ($("#about").length) {
+      self.initAboutPage();
+    }
+  };
+
+  self.initAboutPage = function() {
+    // scrollspy
+    $('body').scrollspy({
+      target: '.steps-sidebar',
+      offset: 100
+    });
+
+    var $lastRow = $('.step-row:last-child');
+
+    // affix
+    $('.nav#sidebar').affix({
+      offset: {
+        top: $('.nav#sidebar').offset().top,
+        // stop affix 2/3 of the way down the last step row
+        bottom: $('body').height() - $lastRow.offset().top - $lastRow.height() * 2 / 3,
+      }
+    });
   };
 
   self.initMaps = function() {
     // load map of a place
-    L.Icon.Default.imagePath = '/static/bower_components/leaflet/dist/images';
+    var $maps = $('.map');
+    if ($maps.length) {
+      L.Icon.Default.imagePath = '/static/bower_components/leaflet/dist/images';
 
-    $('.map').each(function(i, e) {
-      var $map = $(e),
-          coords = [$map.data('lat'), $map.data('lng')];
+      $('.map').each(function(i, e) {
+        var $map = $(e),
+            coords = [$map.data('lat'), $map.data('lng')];
 
-      var map = new L.Map(e, {
-        scrollWheelZoom: false,
-        zoomControl: false,
-        dragging: true,
+        var map = new L.Map(e, {
+          scrollWheelZoom: false,
+          zoomControl: false,
+          dragging: true,
+        });
+        map.attributionControl.setPrefix('');
+        new L.Control.Zoom({position: 'topright'}).addTo(map);
+        new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: 'Map © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          maxZoom: 18
+        }).addTo(map);
+
+        map.setView(coords, 13);
+        L.marker(coords).addTo(map);
       });
-      map.attributionControl.setPrefix('');
-      new L.Control.Zoom({position: 'topright'}).addTo(map);
-      new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 18
-      }).addTo(map);
-
-      map.setView(coords, 13);
-      L.marker(coords).addTo(map);
-    });
+    }
   };
 };
 
@@ -37,30 +75,3 @@ $(function() {
   Umibukela.general = new Umibukela.General();
   Umibukela.general.init();
 });
-
-// function for scrolling around website (links to "#section" will scroll to that section)
-$('a[href^="#"]').on('click',function (e) {
-  e.preventDefault();
-
-  var target = this.hash;
-  var $target = $(target);
-
-  $('html, body').stop().animate({
-      'scrollTop': $target.offset().top-15
-  }, 900, 'swing');
-});
-
-// scrollspy
-$('body').scrollspy({
-  target: '.steps-sidebar',
-  offset: 100
-});
-
-/*
-// affix
-$('.nav#sidebar').affix({
-      offset: {
-        top: $('.nav#sidebar').height()
-      }
-}); 
-*/
