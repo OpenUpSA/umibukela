bc7f9eba295cf1a3d9d8036ac1db993b6a37b58c
JD Bothma <jbothma@gmail.com>
2016-01-11 14:42:01 +0200

    Add very rough, potientially unusable model for field names

---
+++ umibukela/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Partner(models.Model):
+    short_name = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=200)
+    physical_address = models.CharField(max_length=200)
+    contact_person = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=200)
+    email_address = models.EmailField(max_length=200)
+    intro_title = models.CharField(max_length=200)
+    intro_statement = models.CharField(max_length=200)
+    intro_image = models.ImageField()
+    context_quote = models.CharField(max_length=200)
+    context_statement = models.CharField(max_length=200)
+    context_image = models.ImageField()
