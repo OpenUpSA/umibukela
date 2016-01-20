e5fe6a6256a6d2c95ebde79c75ff094dbbe0177f
JD Bothma <jbothma@gmail.com>
2016-01-20 13:46:06 +0200

    Override Site adrmin form to allow location entry via coords

---
+++ umibukela/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.gis.geos import Point
+
+from .models import (
+    Site,
+)
+
+
+class SiteForm(forms.ModelForm):
+
+    latitude = forms.DecimalField(
+        min_value=-90,
+        max_value=90,
+        required=True,
+    )
+    longitude = forms.DecimalField(
+        min_value=-180,
+        max_value=180,
+        required=True,
+    )
+
+    class Meta(object):
+        model = Site
+        exclude = []
+        widgets = {'coordinates': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        if args:    # If args exist
+            data = args[0]
+            if data['latitude'] and data['longitude']:
+                latitude = float(data['latitude'])
+                longitude = float(data['longitude'])
+                data['coordinates'] = Point(longitude, latitude)
+
+        if 'instance' in kwargs and kwargs['instance'] is not None:
+            coordinates = kwargs['instance'].coordinates.tuple
+            initial = kwargs.get('initial', {})
+            initial['longitude'] = coordinates[0]
+            initial['latitude'] = coordinates[1]
+            kwargs['initial'] = initial
+        super(SiteForm, self).__init__(*args, **kwargs)
