47ef3483ae16cc3f0664057c5fa2e51eb632b7fa
JD Bothma <jbothma@gmail.com>
2016-02-03 11:42:30 +0200

    cleverness learnt from importing a second dataset

--- bin/import.py
+++ bin/import.py
@@ -3,7 +3,7 @@ import csv
 
 # pd.unique(facility_coldf.facility.ravel())
 
-crs = models.CycleResultSet.objects.all()[0]
+crs = models.CycleResultSet.objects.filter(site__name__iregex=r'paarl', cycle__name__iregex=r'cycle 2')[0]
 
 f = open('DOH-Citizen-Survey_2016_01_25_04_22_15.csv')
 r = csv.DictReader(f, delimiter=',', quotechar='"')
