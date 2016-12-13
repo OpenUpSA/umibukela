286160b294f21f69109700552285bf0247e06c59
JD Bothma <jbothma@gmail.com>
2016-12-13 10:46:21 +0200

    Looks like kobotoolbox API doesn't support CORS
    and have known about it since May
    http://support.kobotoolbox.org/customer/portal/questions/16222880-cors-not-allowed-on-api

--- umibukela/static/javascript/survey-form.js
+++ umibukela/static/javascript/survey-form.js
@@ -1,1 +1,34 @@
-console.log("survey form lala");
+django.jQuery(document).ready(function() {
+  var $ = django.jQuery;
+
+
+
+  var listProjects = function() {
+    $.ajax('https://kc.kobotoolbox.org/api/v1/projects', {
+      dataType: 'json',
+      headers: {
+        'Authorization': 'Token ' + kobo_access_token
+      },
+      crossDoamin: false,
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(textStatus + '\n' + errorThrown);
+      },
+      success: function(data, textStatus, jqXHR) {
+        var container = $('#project-list-container');
+        container.empty();
+        var select = $('<select />');
+        $.each(data, function(idx, project) {
+          select.append('<option value="' + project.url + '">' + project.name + '</option>');
+        });
+      }
+    });
+  };
+
+  if (kobo_authed) {
+    $('.add-button').removeClass('disabled');
+  } else {
+    $('#kobo-import-container').append('<a href="https://kc.kobotoolbox.org/o/authorize?client_id=' + kobo_client_id + '&response_type=code&scope=read&state=' + path + '">Login to Kobo</a>');
+  }
+
+  $('.add-button').click(listProjects);
+});
