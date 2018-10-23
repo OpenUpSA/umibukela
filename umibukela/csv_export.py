b05caf30aad7912b151f49f038a1bba1a9926098
lunga <stumenz.complex@gmail.com>
2018-10-23 09:51:39 +0200

    remove personal details

--- umibukela/csv_export.py
+++ umibukela/csv_export.py
@@ -1,5 +1,12 @@
 def export_row(answer):
     obj = answer.answers
+    if 'Contact_number' in obj:
+        del obj['Contact_number']
+    if 'Full_name' in obj:
+        del obj['Full_name']
+    if 'Monitor_name' in obj:
+        del obj['Monitor_name']
+
     obj['created_at'] = answer.created_at
     obj['updated_at'] = answer.updated_at
     return obj
