acbf3978b7530321c4e966341296f47aada23b92
lunga <stumenz.complex@gmail.com>
2018-10-23 11:35:08 +0200

    add field to exclude list

--- umibukela/csv_export.py
+++ umibukela/csv_export.py
@@ -6,6 +6,14 @@ def export_row(answer):
         del obj['Full_name']
     if 'Monitor_name' in obj:
         del obj['Monitor_name']
+    if 'phonenumber' in obj:
+        del obj['phonenumber']
+    if 'capturer' in obj:
+        del obj['capturer']
+    if 'surveyor' in obj:
+        del obj['surveyor']
+    if 'Monitor_Name' in obj:
+        del obj['Monitor_Name']
 
     obj['created_at'] = answer.created_at
     obj['updated_at'] = answer.updated_at
