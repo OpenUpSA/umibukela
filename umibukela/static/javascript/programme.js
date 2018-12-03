07a4a64c2c9416480406c7556c4f565475255c55
lunga <stumenz.complex@gmail.com>
2018-12-03 04:04:42 +0200

    fetch resources based on cycle

--- umibukela/static/javascript/programme.js
+++ umibukela/static/javascript/programme.js
@@ -1,6 +1,26 @@
 function fetch_period_results(short_name, cycle_id, date_string){
-	 $.ajax({
-	     url: '/programme/results/'+short_name+'/'+cycle_id,
+    $('#period-name').html(date_string);
+    $('#monitor-period-name').html(date_string);
+    $('#community-period-name').html(date_string);
+    fetch_submission_count(short_name, cycle_id, date_string);
+    fetch_datastudio_results(short_name, cycle_id, date_string);
+    fetch_community_results(short_name, cycle_id, date_string);
+};
+
+function fetch_submission_count(short_name, cycle_id,date_string){
+    $.ajax({
+	url: '/programme/submission/results/'+short_name+'/'+cycle_id,
+	type: 'GET',
+	success: function(data){
+	    $('#submission_count').html(data.submission_count + ' total citzens surveyed');
+	}
+	
+    });
+}
+
+function fetch_datastudio_results(short_name, cycle_id, date_string){
+    $.ajax({
+	     url: '/programme/datastudio/results/'+short_name+'/'+cycle_id,
 	     type: 'GET',
 	     success: function(data){
 		 if (data){
@@ -53,5 +73,59 @@ function fetch_period_results(short_name, cycle_id, date_string){
 	     error: function(data){}
 	     
 	 });
-    
 };
+function fetch_community_results(short_name, cycle_id, date_string){
+	 $.ajax({
+	     url: '/programme/community/results/'+short_name+'/'+cycle_id,
+	     type: 'GET',
+	     success: function(data){
+		 if (data){
+		     $('#community-period-name').html(date_string);
+		     var tbody = document.getElementById('community-body');
+		     if (Object.keys(data.partners).length > 0){
+			 $('#community-body').empty();
+			 for (p in data.partners){
+			     console.log(data.partners[p].name);
+			     var table_row = document.createElement('tr');
+			     var row_name = document.createElement('td');
+			     row_name.innerHTML = data.partners[p].name;
+			     
+			     var row_url = document.createElement('td');
+			     var url = document.createElement('a');
+			     url.setAttribute('href', data.partners[p].url);
+			     url.innerHTML = 'View';
+			     row_url.appendChild(url);
+			     
+			     var row_site = document.createElement('td');
+			     row_site.innerHTML = data.partners[p].site;
+			     var row_total = document.createElement('td');
+			     row_total.innerHTML = data.partners[p].total;
+			     table_row.appendChild(row_name);
+			     table_row.appendChild(row_url);
+			     table_row.appendChild(row_site);
+			     table_row.appendChild(row_total);
+			     tbody.appendChild(table_row);
+			 }
+			 
+			 
+		     }else {
+			 console.log("There are no community results for this period");
+			 $('#community-table').empty();
+			 var h3 = document.createElement('h3');
+			 h3.className='text-center';
+			 h3.appendChild("No Results for this period");
+			 var community_table = document.getElementById('community-table');
+			 community_table.appendChild(h3);
+			 
+			 
+		     }
+		     
+		 }else{
+		     console.log("No data here");
+		 }
+	     },
+	     error: function(data){}
+	     
+	 });
+    
+}
