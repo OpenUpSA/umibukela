34c1a29a429b88c2f574e9120cfe93ba524633da
paulmwatson <github.com@paulmwatson.com>
2023-07-10 16:53:07 +0100

    Change to use Google Tag Manager

--- umibukela/context_processors.py
+++ umibukela/context_processors.py
@@ -8,8 +8,8 @@ def general(request):
         'SITE_NAME': 'Community-Based Monitoring',
     }
 
-    ga_tracking_id = getattr(settings, 'GOOGLE_ANALYTICS_ID', False)
+    ga_tracking_id = getattr(settings, 'GOOGLE_TAG_MANAGER', False)
     if not settings.DEBUG and ga_tracking_id:
-        info['GOOGLE_ANALYTICS_ID'] = ga_tracking_id
+        info['GOOGLE_TAG_MANAGER'] = ga_tracking_id
 
     return info
